d1727ec874c98f93af0c337e4ac0b5237d643918
Sai Dheeraj <saidheeraj.kumar04@gmail.com>
2020-12-27 12:39:10 +0530

    Add progress bar on changing route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,30 @@ import type { AppProps } from 'next/app'
 import AppNavBar from '../components/AppNavBar'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import { Router } from 'next/router'
+import ProgressBar from '@badrap/bar-of-progress'
 
 const queryClient = new QueryClient()
+
+const progress = new ProgressBar({
+  size: 2,
+  color: 'rgb(79, 70, 229)',
+  className: 'bar-of-progress',
+  delay: 100,
+})
+
+// this fixes safari jumping to the bottom of the page
+// when closing the search modal using the `esc` key
+if (typeof window !== 'undefined') {
+  progress.start()
+  progress.finish()
+}
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', () => {
+  progress.finish()
+})
+Router.events.on('routeChangeError', progress.finish)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
