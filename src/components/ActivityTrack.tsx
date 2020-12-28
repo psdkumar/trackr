d422ecbf145dfd6c21eb26dd07359076d2879178
Sai Dheeraj <saidheeraj.kumar04@gmail.com>
2020-12-28 22:59:17 +0530

    Integrate date range picker with chart

--- src/components/ActivityTrack.tsx
+++ src/components/ActivityTrack.tsx
@@ -4,28 +4,64 @@ import { useState } from 'react'
 import { addDays } from 'date-fns'
 import 'react-date-range/dist/styles.css' // main css file
 import 'react-date-range/dist/theme/default.css' // theme css file
+import { Button } from 'coderplex-ui'
 
 export default function ActivityTrack({ activity }) {
-  const [state, setState] = useState([
+  const today = new Date()
+  const defaultStartDate = addDays(today, -today.getDay())
+  const defaultEndDate = addDays(today, 6 - today.getDay())
+  const defaultRange = [
     {
-      startDate: new Date(),
-      endDate: addDays(new Date(), 7),
+      startDate: defaultStartDate,
+      endDate: defaultEndDate,
       key: 'selection',
     },
-  ])
+  ]
+  const [state, setState] = useState(defaultRange)
+  const [startDate, setStartDate] = useState(defaultStartDate)
+  const [endDate, setEndDate] = useState(defaultEndDate)
+
+  console.log({ startDate, endDate })
 
   return (
     <>
-      <h2 className="text-base text-gray-700">
-        Cumulative Activity Tracking Chart
-      </h2>
-      <div className="flex justify-center py-5">
+      <div className="flex justify-between">
+        <h2 className="text-base text-gray-700">
+          Cumulative Activity Tracking Chart
+        </h2>
+        <div className="flex space-x-2">
+          <Button
+            variant="outlined"
+            variantColor="brand"
+            onClick={() => {
+              setState(defaultRange)
+              setStartDate(defaultStartDate)
+              setEndDate(defaultEndDate)
+            }}
+          >
+            Reset to default
+          </Button>
+          <Button
+            variant="solid"
+            variantColor="brand"
+            onClick={() => {
+              setStartDate(state[0].startDate)
+              setEndDate(state[0].endDate)
+            }}
+          >
+            Track
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex justify-center py-5 px-10">
         <DateRangePicker
           editableDateInputs={true}
           showSelectionPreview={true}
           moveRangeOnFirstSelection={false}
           ranges={state}
           onChange={(item) => setState([(item as any).selection])}
+          className="overflow-scroll"
         />
       </div>
       {/* <DateRange
@@ -36,8 +72,8 @@ export default function ActivityTrack({ activity }) {
       /> */}
       <ActivityChart
         activity={activity}
-        startDate={new Date('2020-12-01')}
-        endDate={new Date('2020-12-31')}
+        startDate={startDate}
+        endDate={endDate}
       />
     </>
   )
