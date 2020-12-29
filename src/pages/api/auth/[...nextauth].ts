279f457cb549399399fe9cdc1f0995071aec1d5f
Sai Dheeraj <saidheeraj.kumar04@gmail.com>
2020-12-29 22:37:29 +0530

    remove custom auth verify request page

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -37,7 +37,7 @@ const options: InitOptions = {
     signIn: '/',
     // signOut: '/auth/signout',
     // error: '/auth/error', // Error code passed in query string as ?error=
-    verifyRequest: '/auth/verify-request', // (used for check email message)
+    // verifyRequest: '/auth/verify-request', // (used for check email message)
     // newUser: null, // If set, new users will be directed here on first sign in
   },
 }
